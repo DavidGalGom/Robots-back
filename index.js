38688105bb059cb756d56878834f326ceddd8df0
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-05 18:56:58 +0100

    Added index.js initializing server

---
+++ index.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+require("./database/index");
+const initializeServer = require("./server/index");
+
+const port = process.env.PORT ?? process.env.SERVER_BACK_END ?? 5000;
+
+initializeServer(port);
