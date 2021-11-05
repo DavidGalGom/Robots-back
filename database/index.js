f73c6129b7a7e56e53c0c9440775c4f9bbf54f14
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-05 19:51:30 +0100

    Added connectDB function

---
+++ database/index.js
@@ -0,0 +1,20 @@
+const debug = require("debug")("robots:database");
+
+const chalk = require("chalk");
+const mongoose = require("mongoose");
+
+const connectDB = () =>
+  new Promise((resolve, reject) => {
+    mongoose.connect(process.env.MONGODB_STRING, (error) => {
+      if (error) {
+        debug(chalk.red("Initialization failed"));
+        debug(chalk.red(error.message));
+        reject();
+        return;
+      }
+      debug(chalk.green("Conected to the data base"));
+      resolve();
+    });
+  });
+
+module.exports = connectDB;
