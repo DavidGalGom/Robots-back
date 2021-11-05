5a244f2b770a1db7f18b88abb8743a8c34a9ccea
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-05 19:24:40 +0100

    Added initializaServer module

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ const initializeServer = require("./server/index");
 
 const port = process.env.PORT ?? process.env.SERVER_BACK_END ?? 5000;
 
-initializeServer(port);
+(async () => {
+  try {
+    await connectDB();
+    initializeServer(port);
+  } catch (error) {
+    process.exit(1);
+  }
+})();
