2a9d19723859db49b7d12db553e4a2b2299846bb
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-06 13:19:53 +0100

    Added get findById method

--- server/routes/robotsRoutes.js
+++ server/routes/robotsRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { getRobots } = require("../controller/robotsController");
+const { getRobots, getRobotById } = require("../controller/robotsController");
 
 const router = express.Router();
 
 router.get("/", getRobots);
+router.get("/:idRobot", getRobotById);
 module.exports = router;
