6d6338c761043b1165df1ee6d99f0a814bd292de
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-08 18:50:15 +0100

    Fixed different auth scenarios

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,20 +1,20 @@
 const jwt = require("jsonwebtoken");
 
 const auth = async (req, res, next) => {
-  const [, token] = req.headers.authorization.split(" ");
-
-  if (!token) {
+  const authExist = req.headers.authorization;
+  if (!authExist) {
     const error = new Error("Empty token");
     error.code = 401;
     next(error);
   } else {
+    const token = authExist.split(" ")[1];
     try {
       const user = await jwt.verify(token, process.env.USER_CHECK_TOKEN);
       req.userName = user.userName;
       next();
     } catch (error) {
+      error.message = "Wrong token";
       error.code = 401;
-      error.message = "Not a correct token";
       next(error);
     }
   }
