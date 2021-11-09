bbc0ea272fa8c6c9c4d86c1aa59b20b2cf5b7043
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-09 18:41:54 +0100

    Fixed auth.js header authorization

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,21 +1,27 @@
 const jwt = require("jsonwebtoken");
 
 const auth = async (req, res, next) => {
-  const authExist = req.headers.authorization;
+  const authExist = req.header("Authorization");
   if (!authExist) {
-    const error = new Error("Empty token");
+    const error = new Error("Authorization missing");
     error.code = 401;
     next(error);
   } else {
     const token = authExist.split(" ")[1];
-    try {
-      const user = await jwt.verify(token, process.env.USER_CHECK_TOKEN);
-      req.userName = user.userName;
-      next();
-    } catch (error) {
-      error.message = "Wrong token";
+    if (!token) {
+      const error = new Error("Token missing");
       error.code = 401;
       next(error);
+    } else {
+      try {
+        const user = jwt.verify(token, process.env.SECRET_TOKEN);
+        req.userId = user.id;
+        next();
+      } catch (error) {
+        error.message = "Wrong token";
+        error.code = 401;
+        next(error);
+      }
     }
   }
 };
